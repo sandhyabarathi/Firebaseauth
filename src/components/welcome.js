ed777518c06b321c0e51003bb6b3ec3020761c5c
sandhyabarathi <sandhyabarathi2003@gmail.com>
2023-03-26 21:38:33 +0530

    updated

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { signInWithEmailAndPassword , onAuthStateChanged, createUserWithEmailAndPassword} from 'firebase/auth';
 import  { auth } from "../firebase.js";
 import {useNavigate} from "react-router-dom";
-import e from 'express';
+
 
 export default function Welcome() {
   const [email, setEmail] = useState('');
@@ -49,6 +49,15 @@ export default function Welcome() {
   }
 
   const handleRegister = () => {
+    if(registerInfo.email!==registerInfo.confirmEmail)
+    {
+      alert("Email doesn't match!!")
+      return;
+    } else if(registerInfo.password!==registerInfo.confirmPassword)
+    {
+      alert("Password doesn't match!!")
+      return;
+    }
     createUserWithEmailAndPassword(auth, email, password).then(()=>{
       navigate('/homepage')
     }).catch((err)=> alert(err.message));
@@ -64,10 +73,10 @@ export default function Welcome() {
         {
           isRegistering ? (
             <>
-        <input type="email" placeholder='Email' value={registerInfo.email} />
-        <input type="email" placeholder='Confirm Email' value={registerInfo.confirmEmail}/>
-        <input type="password" placeholder='password' value={registerInfo.password}/>
-        <input type="confirm-password" placeholder='Confirm Password' value={registerInfo.confirmPassword}/>
+        <input type="email" placeholder='Email'value={registerInfo.email} onChange={(e)=>setRegisterInfo({...registerInfo,email:e.target.value})}/>
+        <input type="email" placeholder='Confirm Email' value={registerInfo.confirmEmail} onChange={(e)=>setRegisterInfo({...registerInfo,confirmEmail:e.target.value})}/>
+        <input type="password" placeholder='Password' value={registerInfo.password} onChange={(e)=>setRegisterInfo({...registerInfo,password:e.target.value})}/>
+        <input type="confirm-password" placeholder='Confirm Password' value={registerInfo.confirmPassword} onChange={(e)=>setRegisterInfo({...registerInfo,confirmPassword:e.target.value})}/>
         <button onClick={handleRegister}>REGISTER</button>
         <button onClick={() => setIsRegistering(false)}>GO BACK</button>
 
